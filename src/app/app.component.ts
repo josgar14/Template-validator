3a58b9ac83c8ba5a0c1c844ce8f868247f28f4a4
Josue <josgar_14@hotmail.com>
2019-10-06 03:18:34 -0400

    Temlplate validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
+import { NG_VALIDATORS, FormControl } from '@angular/forms';
+
+export function ssnValidator(control: FormControl): { [key: string]: any } {
+	const value: string = control.value || '';
+	const valid = value.match(/^\d{9}$/);
+	return valid ? null : { ssn: true };
+}
+
+@Directive({
+	selector: '[ssn]',
+	providers: [ { provide: NG_VALIDATORS, useValue: ssnValidator, multi: true } ]
+})
+export class SsnValidatorDirective {}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'template-validator';
+	title = 'template-validator';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent, SsnValidatorDirective } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, SsnValidatorDirective ],
+	imports: [ BrowserModule, AppRoutingModule, FormsModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
